1d2b83f5cdf5d08bd6f9efc81a9ce438db98e76c
Ricardo Machado <ricardo.machado.nwi@gmail.com>
2026-04-20 16:00:04 -0300

    Wire win screen play again and space to menu

---
+++ scenes/game-level/win.gd
@@ -0,0 +1,32 @@
+extends GameLevel
+
+const MENU_SCENE_PATH := "res://menu.tscn"
+
+@onready var _play_again_button: Button = $"WinUI/MarginContainer/VBoxContainer/Button"
+
+var _is_returning_to_menu: bool = false
+
+
+func _ready() -> void:
+	super._ready()
+	_play_again_button.pressed.connect(_return_to_menu)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if _is_returning_to_menu:
+		return
+
+	if not event.is_pressed() or event.is_echo():
+		return
+
+	if event is InputEventKey and event.keycode == KEY_SPACE:
+		_return_to_menu()
+		get_viewport().set_input_as_handled()
+
+
+func _return_to_menu() -> void:
+	if _is_returning_to_menu:
+		return
+
+	_is_returning_to_menu = true
+	get_tree().change_scene_to_file(MENU_SCENE_PATH)
